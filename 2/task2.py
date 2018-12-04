d20e750dd135982299a5c69d7962b26627a1dd23
Frikeer <o.flinck@gmail.com>
2018-12-04 17:21:06 +0100

    Solved 2.2

--- 2/task2.py
+++ 2/task2.py
@@ -3,8 +3,22 @@ from collections import Counter
 
 df = pd.read_csv('task2_input.csv', names=['Strings'])
 
-times = {2: 0, 3: 0}
+times = {'doubles': 0, 'triples': 0}
 
-for line in df:
-    letter_count = {}
-    Counter
+for line in df['Strings']:
+    two = False
+    three = False
+    c = dict(Counter(line))
+    # print(c)
+    for l, count in c.items():
+        if count == 3:
+            three = True
+        if count == 2:
+            two = True
+    if two:
+        times['doubles'] = times['doubles'] + 1
+    if three:
+        times['triples'] = times['triples'] + 1
+
+print(times)
+print(times['doubles'] * times['triples'])
