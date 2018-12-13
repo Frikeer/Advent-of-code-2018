0a312929f70c764ef5d4894fe84656f1b4a18b6d
Frikeer <o.flinck@gmail.com>
2018-12-13 16:31:16 +0100

    Task 8 solved

---
+++ 8/task8.py
@@ -0,0 +1,55 @@
+import sys
+sys.getrecursionlimit()
+sys.setrecursionlimit(100000)
+
+with open('8/task8_input.txt', 'r') as f:
+    input = f.read()
+
+input = input.strip('\n')
+input = input.split(' ')
+input = [int(i) for i in input]
+
+
+def findMetaSum(list):
+    # print(list)
+    children = list.pop(0)
+    meta = list.pop(0)
+    mdsum = 0
+
+    for i in range(children):
+        mdsum += findMetaSum(list)
+
+    for i in range(meta):
+        mdsum += list.pop(0)
+    # print('Sum: ' + str(mdsum))
+    return mdsum
+
+
+print("Task 1: " + findMetaSum(input))
+
+
+def nodeValue(list):
+    children = list.pop(0)
+    meta = list.pop(0)
+    value = 0
+    meta_list = []
+    values = {}
+
+    if children == 0:
+        for i in range(meta):
+            value += list.pop(0)
+    else:
+        for i in range(children):
+            values[i + 1] = nodeValue(list)
+
+        for i in range(meta):
+            meta_list.append(list.pop(0))
+
+        for i in meta_list:
+            if i <= children and i > 0:
+                value += values[i]
+
+    return value
+
+
+print("Task 2: " + str(nodeValue(input)))
